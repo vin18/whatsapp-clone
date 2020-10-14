2df005af34379adc9bbf740a925888b5c2ae7d9b
Vinit Raut <vinit.coder@gmail.com>
2020-10-15 01:05:56 +0530

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDMkm_bHmOpKi3dE3SsZRuJJ3ID3OLmYAM',
+  authDomain: 'whatsapp-clone-1e6d1.firebaseapp.com',
+  databaseURL: 'https://whatsapp-clone-1e6d1.firebaseio.com',
+  projectId: 'whatsapp-clone-1e6d1',
+  storageBucket: 'whatsapp-clone-1e6d1.appspot.com',
+  messagingSenderId: '696265609187',
+  appId: '1:696265609187:web:14c1144bb2c1d9f3041ad6',
+};
