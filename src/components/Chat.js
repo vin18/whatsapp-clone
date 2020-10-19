b59e9e965d5704095b134e53a792750e859b9c8f
Vinit Raut <vinit.coder@gmail.com>
2020-10-19 20:57:19 +0530

    added header for Chat component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import { StyledChat } from '../styled/Chat';
+import React, { useState, useEffect } from 'react';
+import { Avatar, IconButton } from '@material-ui/core';
+import {
+  StyledChat,
+  StyledChatHeader,
+  StyledChatHeaderInfo,
+  StyledChatHeaderRight,
+} from '../styled/Chat';
+import {
+  SearchOutlined,
+  AttachFile,
+  MoreVert,
+} from '@material-ui/icons';
 
 const Chat = () => {
+  const [seed, setSeed] = useState('');
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
   return (
     <StyledChat>
-      <h1>Chat</h1>
+      <StyledChatHeader>
+        <Avatar
+          src={`https://avatars.dicebear.com/api/human/${seed}.svg`}
+        />
+
+        <StyledChatHeaderInfo>
+          <h3>Room name</h3>
+          <p>Last seen at ...</p>
+        </StyledChatHeaderInfo>
+
+        <StyledChatHeaderRight>
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </StyledChatHeaderRight>
+      </StyledChatHeader>
     </StyledChat>
   );
 };
--- src/styled/Chat.js
+++ src/styled/Chat.js
@@ -3,3 +3,34 @@ import styled from 'styled-components';
 export const StyledChat = styled.div`
   flex: 0.7;
 `;
+
+export const StyledChatHeader = styled.div`
+  padding: 1.5rem;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid lightgray;
+`;
+
+export const StyledChatHeaderInfo = styled.div`
+  flex: 1;
+  padding-left: 2rem;
+
+  & > h3 {
+    margin-bottom: 0.3rem;
+    font-weight: 500;
+  }
+
+  & > p {
+    color: gray;
+  }
+`;
+
+export const StyledChatHeaderRight = styled.div`
+  display: flex;
+  justify-content: space-between;
+  min-width: 10rem;
+`;
+
+export const StyledChatBody = styled.div``;
+
+export const StyledChatFooter = styled.div``;
